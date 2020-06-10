d49547f47d75343a96d9483ce5c755cf4a87f7b4
Lupo <w-pfeiffer-tue@gmx.de>
2020-06-10 18:41:33 +0200

    Fixing Missing Collider Problem in GameScene

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -21,22 +21,26 @@ export class GameScene extends Phaser.Scene {
         const map = this.make.tilemap({key:'MyTilemap', tileWidth: 32, tileHeight: 32});
         // param tilesetName from TILED Editor
         const tileset = map.addTilesetImage('MyBasicTileset');
-        // param layerID from TILED Editor
-        const platforms = map.createStaticLayer('Floors', tileset, 0, -32);
-        platforms.setCollisionBetween(1, 18,  true);
-
-        const ladders = map.createStaticLayer('Ladders', tileset, 0, -32);
-        ladders.setCollision([19,20], false);
 
         this.add.text(100, 260, 'Game Scene', {fontFamily: "Verdana", fontSize: 40, fontStyle: "italic"});
         this.player = new Player({scene: this, x: 10, y: 440} );
         this.player.cursors = this.input.keyboard.createCursorKeys();
         // https://www.html5gamedevs.com/topic/40809-how-to-alter-player-hitbox/
         this.player.setSize(16,34);
-
         this.player.setCollideWorldBounds(true);
+
+        // param layerID from TILED Editor
+        const platforms = map.createStaticLayer('Floors', tileset, 0, -32);
+        platforms.setCollisionByExclusion([-1], true);
         this.physics.add.collider(this.player, platforms);
+
+        // param layerID from TILED Editor
+        const ladders = map.createStaticLayer('Ladders', tileset, 0, -32);
+        ladders.setCollision([18,19], false);
+        this.physics.add.collider(this.player, ladders);
         ladders.setTileIndexCallback([19,20], this.player.onLadder, this.player);
+
+        this.player.depth = 1;
 
         // Activate for Old-School Feelings:
         //
